7597ea150a6e37af9edfb3526bf29d8c333f0c43
ashdeepupadhyay <ashdeepupadhyay@gmail.com>
2019-05-14 00:53:43 +0530

    os module functionality

---
+++ basicnode/os_demo.js
@@ -0,0 +1,23 @@
+const os=require('os');
+
+
+//Platform
+console.log(os.platform());
+
+//CPU arch
+console.log(os.arch());
+
+//CPU core info
+console.log(os.cpus());
+
+//Free Memory
+console.log(os.freemem());
+
+//Total memory
+console.log(os.totalmem());
+
+//home dir
+console.log(os.homedir());
+
+//UpTime
+console.log(os.uptime());
